12cbc57811707090764989be792c4120dcf13e77
9vult <me@ninevolt.moe>
2026-06-16 11:26:29 -0400

    Audio Drag2Time functionality
    Closes #114

--- Ameko/Views/Controls/TabItemAudioArea.axaml.cs
+++ Ameko/Views/Controls/TabItemAudioArea.axaml.cs
@@ -8,6 +8,7 @@ using AssCS;
 using AssCS.History;
 using Avalonia;
 using Avalonia.Input;
+using Holo;
 using Holo.Models;
 using ReactiveUI;
 using ReactiveUI.Avalonia;
@@ -16,6 +17,22 @@ namespace Ameko.Views.Controls;
 
 public partial class TabItemAudioArea : ReactiveUserControl<TabItemViewModel>
 {
+    /// <summary>
+    /// Types of dragging
+    /// </summary>
+    private enum DragMode
+    {
+        None,
+        SetStart,
+        SetEnd,
+        EdgeStart,
+        EdgeEnd,
+        Seek,
+    }
+
+    private const double EdgeHitPx = 6d;
+    private DragMode _dragMode = DragMode.None;
+
     public TabItemAudioArea()
     {
         InitializeComponent();
@@ -65,36 +82,48 @@ public partial class TabItemAudioArea : ReactiveUserControl<TabItemViewModel>
         if (wsp is null || !wsp.MediaController.IsVideoLoaded || !wsp.MediaController.IsAudioLoaded)
             return;
 
-        var x = e.GetPosition(this).X;
-
-        var mc = wsp.MediaController;
-        var ms = Convert.ToInt64(x * mc.VisualizerScaleX + mc.VisualizerPositionMs);
-
-        // When shift is pressed, don't snap
-        // TODO: Add a config option for this
-        var time =
-            (e.KeyModifiers & KeyModifiers.Shift) == 0
-                ? Time.FromMillis(mc.VideoInfo.MidpointFromMillis(ms))
-                : Time.FromMillis(ms);
-
-        var frame = mc.VideoInfo.FrameFromTime(time);
+        var modifiers = e.KeyModifiers;
         var @event = wsp.SelectionManager.ActiveEvent;
+        var x = e.GetPosition(this).X;
 
         switch (e.Properties.PointerUpdateKind)
         {
             case PointerUpdateKind.LeftButtonPressed:
-                if (time < @event.End)
+            {
+                // Check if we're near the edge of the active event
+                var startX = TimeToPosition(@event.Start);
+                var endX = TimeToPosition(@event.End);
+
+                if (Math.Abs(x - startX) <= EdgeHitPx) // Drag start
                 {
-                    @event.Start = time;
+                    _dragMode = DragMode.EdgeStart;
                 }
-                else
+                else if (Math.Abs(x - endX) <= EdgeHitPx) // Drag end
                 {
-                    @event.Start = @event.End;
-                    @event.End = time;
+                    _dragMode = DragMode.EdgeEnd;
                 }
-                wsp.Commit(@event, ChangeType.ModifyEventMeta);
+                else // Set start
+                {
+                    _dragMode = DragMode.SetStart;
+                    var time = PositionToTime(x, modifiers);
+
+                    if (time < @event.End)
+                    {
+                        @event.Start = time;
+                    }
+                    else
+                    {
+                        @event.Start = @event.End;
+                        @event.End = time;
+                    }
+                }
+                e.Pointer.Capture(sender as IInputElement);
                 break;
+            }
             case PointerUpdateKind.RightButtonPressed:
+            {
+                _dragMode = DragMode.SetEnd;
+                var time = PositionToTime(x, modifiers);
                 if (time > @event.Start)
                 {
                     @event.End = time;
@@ -104,11 +133,158 @@ public partial class TabItemAudioArea : ReactiveUserControl<TabItemViewModel>
                     @event.End = @event.Start;
                     @event.Start = time;
                 }
-                wsp.Commit(@event, ChangeType.ModifyEventMeta);
+                e.Pointer.Capture(sender as IInputElement);
                 break;
+            }
             case PointerUpdateKind.MiddleButtonPressed:
+            {
+                _dragMode = DragMode.Seek;
+                var time = PositionToTime(x, modifiers);
+                var frame = wsp.MediaController.VideoInfo.FrameFromTime(time);
+                wsp.MediaController.SeekTo(frame);
+
+                e.Pointer.Capture(sender as IInputElement);
+                break;
+            }
+        }
+    }
+
+    private void AudioTarget_OnPointerMoved(object? sender, PointerEventArgs e)
+    {
+        e.Handled = true;
+        var wsp = ViewModel?.Workspace;
+        if (wsp is null || !wsp.MediaController.IsVideoLoaded || !wsp.MediaController.IsAudioLoaded)
+            return;
+
+        var modifiers = e.KeyModifiers;
+        var @event = wsp.SelectionManager.ActiveEvent;
+        var x = e.GetPosition(this).X;
+
+        if (_dragMode is DragMode.None)
+        {
+            if (sender is InputElement element)
+            {
+                var startX = TimeToPosition(@event.Start);
+                var endX = TimeToPosition(@event.End);
+                if (Math.Abs(x - startX) <= EdgeHitPx || Math.Abs(x - endX) <= EdgeHitPx)
+                    element.Cursor = new Cursor(StandardCursorType.SizeWestEast);
+                else
+                    element.Cursor = Cursor.Default;
+            }
+            return;
+        }
+
+        var time = PositionToTime(x, modifiers);
+        switch (_dragMode)
+        {
+            case DragMode.SetStart:
+                if (time < @event.End)
+                {
+                    @event.Start = time;
+                }
+                else
+                {
+                    @event.Start = @event.End;
+                    @event.End = time;
+                }
+                break;
+            case DragMode.SetEnd:
+                if (time > @event.Start)
+                {
+                    @event.End = time;
+                }
+                else
+                {
+                    @event.End = @event.Start;
+                    @event.Start = time;
+                }
+                break;
+            case DragMode.EdgeStart:
+                @event.Start = time < @event.End ? time : @event.End;
+                break;
+            case DragMode.EdgeEnd:
+                @event.End = time > @event.Start ? time : @event.Start;
+                break;
+            case DragMode.Seek:
+                var frame = wsp.MediaController.VideoInfo.FrameFromTime(time);
                 wsp.MediaController.SeekTo(frame);
                 break;
         }
+    }
+
+    private void AudioTarget_OnPointerReleased(object? sender, PointerReleasedEventArgs e)
+    {
+        switch (_dragMode)
+        {
+            case DragMode.None:
+                return;
+            case DragMode.Seek:
+            {
+                _dragMode = DragMode.None;
+                e.Pointer.Capture(null);
+
+                if (sender is InputElement element)
+                    element.Cursor = Cursor.Default;
+                return;
+            }
+            default:
+            {
+                var wsp = ViewModel?.Workspace;
+                wsp?.Commit(wsp.SelectionManager.ActiveEvent, ChangeType.ModifyEventMeta);
+
+                _dragMode = DragMode.None;
+                e.Pointer.Capture(null);
+
+                if (sender is InputElement element)
+                    element.Cursor = Cursor.Default;
+                return;
+            }
+        }
+    }
+
+    private Time PositionToTime(double x, KeyModifiers modifiers)
+    {
+        // When shift is pressed, don't snap
+        // TODO: Add a config option for this
+
+        var mc = ViewModel!.Workspace.MediaController;
+
+        var startMs = GetClampedPositionMs(mc);
+        var ms = Convert.ToInt64(x * mc.VisualizerScaleX + startMs);
+
+        return (modifiers & KeyModifiers.Shift) == 0
+            ? Time.FromMillis(mc.VideoInfo!.MidpointFromMillis(ms))
+            : Time.FromMillis(ms);
+    }
+
+    private double TimeToPosition(Time time)
+    {
+        var mc = ViewModel!.Workspace.MediaController;
+        var startMs = GetClampedPositionMs(mc);
+        return (time.TotalMilliseconds - startMs) / mc.VisualizerScaleX;
+    }
+
+    /// <summary>
+    /// Re-implementation of the clamping code in Mizuki so we don't scroll past the end
+    /// </summary>
+    private static double GetClampedPositionMs(MediaController mc)
+    {
+        var totalDurationMs = mc.AudioInfo!.Duration;
+        var visibleDurationMs = mc.VisualizerWidth * mc.VisualizerScaleX;
+
+        var startMs = Convert.ToDouble(mc.VisualizerPositionMs);
+
+        if (totalDurationMs > visibleDurationMs)
+        {
+            var maxStartMs = totalDurationMs - visibleDurationMs;
+            if (startMs > maxStartMs)
+                startMs = maxStartMs;
+        }
+        else
+        {
+            startMs = 0.0;
+        }
+
+        return startMs;
     }
 }
