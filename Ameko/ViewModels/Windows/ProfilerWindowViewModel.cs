fc2a9452c1bf72bcb2c61ba9c809271e01700c7d
9vult <me@ninevolt.moe>
2026-06-25 20:07:46 -0400

    WIP: profiler window

---
+++ Ameko/ViewModels/Windows/ProfilerWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿// SPDX-License-Identifier: GPL-3.0-only
+
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Holo;
+using Holo.Models;
+using Holo.Providers;
+
+namespace Ameko.ViewModels.Windows;
+
+public class ProfilerWindowViewModel : ViewModelBase
+{
+    public IProjectProvider ProjectProvider { get; set; }
+
+    public ReadOnlyObservableCollection<Workspace> Workspaces { get; }
+    public Dictionary<Workspace, ProfileResult?> Results { get; }
+    public Workspace? SelectedWorkspace { get; set; }
+
+    public ProfilerWindowViewModel(IProjectProvider projectProvider)
+    {
+        ProjectProvider = projectProvider;
+        Workspaces = projectProvider.Current.LoadedWorkspaces;
+        SelectedWorkspace = projectProvider.Current.WorkingSpace;
+        Results = [];
+    }
+}
