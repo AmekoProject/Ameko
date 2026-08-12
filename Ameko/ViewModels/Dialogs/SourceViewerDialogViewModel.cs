6567e05269bacb87cf5beeeba9a6a99ec9ec3cbe
9vult <me@ninevolt.moe>
2026-08-12 10:07:55 -0400

    Start on source viewer dialog

---
+++ Ameko/ViewModels/Dialogs/SourceViewerDialogViewModel.cs
@@ -0,0 +1,50 @@
+﻿// SPDX-License-Identifier: GPL-3.0-only
+
+using System;
+using System.IO;
+using System.Windows.Input;
+using AvaloniaEdit.Document;
+using Microsoft.Extensions.Logging;
+using ReactiveUI;
+
+namespace Ameko.ViewModels.Dialogs;
+
+public class SourceViewerDialogViewModel : ViewModelBase
+{
+    public bool IsLocalScript { get; }
+    public TextDocument Document { get; }
+    public ICommand SaveCommand { get; }
+
+    public SourceViewerDialogViewModel(
+        ILogger<SourceViewerDialogViewModel> logger,
+        Uri scriptUri,
+        string scriptContent
+    )
+    {
+        IsLocalScript = scriptUri.IsFile;
+        Document = new TextDocument(scriptContent);
+
+        SaveCommand = ReactiveCommand.CreateFromTask(async () =>
+        {
+            // Just in case
+            if (!IsLocalScript)
+                return;
+
+            try
+            {
+                await using var fs = new FileStream(
+                    scriptUri.LocalPath,
+                    FileMode.Create,
+                    FileAccess.Write,
+                    FileShare.None
+                );
+                await using var writer = new StreamWriter(fs);
+                await writer.WriteAsync(Document.Text);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Failed to write to {ScriptPath}", scriptUri.LocalPath);
+            }
+        });
+    }
+}
