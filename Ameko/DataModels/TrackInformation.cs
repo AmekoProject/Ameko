1ff12ae0bf7849faafc23228566bfc450a143b98
9vult <me@ninevolt.moe>
2026-09-08 14:47:34 -0400

    Include track metadata in selection dialog
    Closes #135

--- Ameko/DataModels/TrackInformation.cs
+++ Ameko/DataModels/TrackInformation.cs
@@ -11,4 +11,16 @@ public class TrackInformation
     public required string Codec { get; init; }
     public required string Language { get; init; }
     public required string Title { get; init; }
+
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        return (string.IsNullOrEmpty(Language), string.IsNullOrEmpty(Title)) switch
+        {
+            (true, true) => $"{Index}. {Codec}",
+            (false, false) => $"{Index}. {Codec} ({Language}, {Title})",
+            (true, false) => $"{Index}. {Codec} ({Title})",
+            (false, true) => $"{Index}. {Codec} ({Language})",
+        };
+    }
 }
